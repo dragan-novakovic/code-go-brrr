e36790381451016fd2d2deae9e5c4be27e6a27d3
dragan1810 <d.novakovic1810@yahoo.com>
2022-04-29 18:28:12 +0200

    111

---
+++ src/algo/two_sum.rs
@@ -0,0 +1,21 @@
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut first_num = 99;
+    let mut second_num = 99;
+
+    'outer: for (i, v) in nums.iter().enumerate() {
+        first_num = i as i32;
+        dbg!(i);
+
+        if i + 1 <= nums.len() {
+            for j in i + 1..nums.len() {
+                if v + nums[j] == target {
+                    dbg!(j);
+                    second_num = j as i32;
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    vec![first_num, second_num]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let result: Vec<i32> = nums
-        .iter()
-        .enumerate()
-        .fold(vec![], |mut acc, (index, value)| {
-            nums.iter().enumerate().for_each(|(inn_index, inner_val)| {
-                if value + inner_val == target {
-                    dbg!(value, inner_val);
-                    acc.push(index as i32);
-                    acc.push(inn_index as i32);
-                }
-            });
-            acc
-        });
+    let mut first_num = 99;
+    let mut second_num = 99;
 
-    result
+    'outer: for (i, v) in nums.iter().enumerate() {
+        first_num = i as i32;
+        dbg!(i);
+
+        if i + 1 <= nums.len() {
+            for j in i + 1..nums.len() {
+                if v + nums[j] == target {
+                    dbg!(j);
+                    second_num = j as i32;
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    vec![first_num, second_num]
 }
 
 fn main() {
-    let result = two_sum(vec![3, 2, 3, 5, 7, 10], 6);
+    let result = two_sum(vec![3, 2, 2, 5, 7, 3], 6);
     dbg!(result);
 }
