742eaba0045b4ffdab03f6f7bea41146ebda193e
dragan1810 <d.novakovic1810@yahoo.com>
2022-05-02 21:30:27 +0200

    qqq

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,52 @@
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut first_num = 99;
-    let mut second_num = 99;
+fn main() {
+    struct LRUCache {
+        capacity: i32,
+        keys: Vec<i32>,
+        prev_keys: Vec<i32>,
+        values: Vec<i32>,
+    }
 
-    'outer: for (i, v) in nums.iter().enumerate() {
-        first_num = i as i32;
-        dbg!(i);
+    /**
+     * `&self` means the method takes an immutable reference.
+     * If you need a mutable reference, change it to `&mut self` instead.
+     */
+    impl LRUCache {
+        fn new(capacity: i32) -> Self {
+            LRUCache {
+                capacity,
+                keys: vec![],
+                prev_keys: vec![],
+                values: vec![],
+            }
+        }
 
-        if i + 1 <= nums.len() {
-            for j in i + 1..nums.len() {
-                if v + nums[j] == target {
-                    dbg!(j);
-                    second_num = j as i32;
-                    break 'outer;
+        fn get(&mut self, key: i32) -> i32 {
+            match self.keys.iter().position(|k| k == &key) {
+                Some(x) => {
+                    self.prev_keys.push(key);
+                    println!("STO ME ZAJ, {:?}", self.prev_keys);
+                    self.values[x]
                 }
+                None => -1,
+            }
+        }
+
+        fn put(&mut self, key: i32, value: i32) {
+            if (self.keys.len() as i32) < self.capacity {
+                self.keys.push(key);
+                self.values.push(value);
             }
         }
     }
 
-    vec![first_num, second_num]
-}
-
-fn main() {
-    let result = two_sum(vec![3, 2, 2, 5, 7, 3], 6);
-    dbg!(result);
+    let mut lRUCache = LRUCache::new(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1); // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2); // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1); // return -1 (not found)
+    lRUCache.get(3); // return 3
+    lRUCache.get(4); // return 4
 }
