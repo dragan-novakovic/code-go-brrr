e9c82e68171d1afde28ec01c752042180e76ddf3
dragan1810 <d.novakovic1810@yahoo.com>
2022-06-07 22:49:00 +0200

    qq

--- src/algo/insertion_basic.rs
+++ src/algo/insertion_basic.rs
@@ -1,18 +1,14 @@
-pub fn _sort(list: Vec<i32>) -> Vec<i32> {
-    let out = vec![];
-
-    let mut current: i32;
-    for (i, val) in list.iter().enumerate() {
+pub fn _sort(mut list: Vec<i32>) -> Vec<i32> {
+    for (mut i, _val) in list.clone().iter_mut().enumerate() {
         if i > 0 {
-            current = list[i];
-            dbg!(i);
-            let j = i - 1;
-            while j >= 0 && list[j] < list[i] {
-                //
+            let mut j = i - 1;
+            while (j >= 0) && (list[j] > list[i]) {
+                list[j + 1] = list[i];
+                j = j - 1;
             }
         }
     }
 
-    dbg!(&out);
-    out
+    dbg!(&list.clone());
+    list.clone()
 }
